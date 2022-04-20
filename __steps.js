3a9645eb69e10eb5789ec301b9ea6b79472b53b3
MehruzSaif <mehruzsaif007@gmail.com>
2022-04-21 02:22:25 +0600

    set app.get{'/) and listen to port on index.js'

--- __steps.js
+++ __steps.js
@@ -1,15 +1,15 @@
 /**
  * ---------------------
  * Basic server setup
- * --------------------
+ * ---------------------
  * 1. create a folder (manually or mkdir)
  * 2. open command line to that folder
  * 3. npm init
  * 4. npm install express
  * 5. create index.js
- * --------------------
+ * ------------------------
  * For Express/node server
- * --------------------
+ * ------------------------
  * 1. require express
  * 2. create app variable
  * 3. declare port
@@ -17,13 +17,13 @@
  * 5. listen to port
  * 6. node index.js
  * 7. check your browser for that port
- * -----------
+ * ----
  * get
- * -----------
+ * ----
  * 1. cors middleware
- * ---------------------
+ * -----
  * post
- * -----------------------
+ * -----
  * 1.
  * 
 */
